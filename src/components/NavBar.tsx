ad4efb6fa18e5ff216efdc388c73b848bd95bea3
justynpollard1 <72589071+justynpollard1@users.noreply.github.com>
2025-02-28 12:04:55 -0600

    replace icons with tabler icon components.
    Could not find matching github icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,10 @@
 import { useState } from "preact/hooks";
 import "../styles/global.css";
-import discordIcon from "../assets/icons/discord-icon.svg?url";
-import downloadIcon from "../assets/icons/download-icon.svg?url";
+import { IconDownload } from "@tabler/icons-preact";
+import { IconBrandDiscordFilled } from "@tabler/icons-preact";
+import { IconBaselineDensityLarge } from "@tabler/icons-preact";
+import { IconX } from "@tabler/icons-preact";
 import githubIcon from "../assets/icons/github-icon.svg?url";
-import hamburgerIcon from "../assets/icons/hamburger-icon.svg?url";
-import xIcon from "../assets/icons/x-icon.svg?url";
 
 export default function NavBar() {
   const [open, setOpen] = useState(false);
@@ -16,46 +16,46 @@ export default function NavBar() {
             <img src="/gtnh-simple.svg" />
             <h1 className="text-white font-mono">New Horizons</h1>
           </a>
-          <div className="flex gap-x-6 max-md:hidden">
+          <div className="flex gap-x-4 max-md:hidden">
             <a href="downloads" className="flex gap-x-1.5 items-center">
-              <img src={downloadIcon} className="w-4 h-3" />
+              <IconDownload width={16} height={12} color="#999999"/>
               <span className="text-[#B3B3B3] text-xs">Downloads</span>
             </a>
-            <a href="mod-list" className="text-[#B3B3B3] text-xs">
+            <a href="mod-list" className="text-text-400 text-xs">
               Mod List
             </a>
-            <a href="server" className="text-[#B3B3B3] text-xs">
+            <a href="server" className="text-text-400 text-xs">
               Server
             </a>
-            <a href="team" className="text-[#B3B3B3] text-xs">
+            <a href="team" className="text-text-400 text-xs">
               Team
             </a>
-            <a href="faq" className="text-[#B3B3B3] text-xs">
+            <a href="faq" className="text-text-400 text-xs">
               FAQ
             </a>
-            <a href="#" className="text-[#B3B3B3] text-xs">
+            <a href="#" className="text-text-400 text-xs">
               Wiki
             </a>
             <a
               href="https://github.com/GTNewHorizons"
               className="flex gap-x-1.5 items-center"
             >
-              <img src={githubIcon} className="size-3" />
+              <img src={githubIcon} className="size-3 color-" />
               <span className="text-[#B3B3B3] text-xs">Github</span>
             </a>
             <a
               href="https://discord.com/invite/EXshrPV"
               className="flex gap-x-1.5 items-center"
             >
-              <img src={discordIcon} className="w-4 h-3" />
+              <IconBrandDiscordFilled width={16} height={12} color="#999999"/>
               <span className="text-[#B3B3B3] text-xs">Discord</span>
             </a>
           </div>
           <button className="flex md:hidden" onClick={() => setOpen(!open)}>
             {!open ? (
-              <img src={hamburgerIcon} className="w-4 h-2" />
+              <IconBaselineDensityLarge width={16} height={2} color="#999999"/>
             ) : (
-              <img src={xIcon} className="size-3" />
+              <IconX stroke={2} width={12} height={12} color="#999999"/>
             )}
           </button>
         </div>
@@ -65,58 +65,58 @@ export default function NavBar() {
           <div className="grid grid-cols-2 px-4 gap-y-8 self-center w-full">
             <div className="space-y-2">
               <h2 className="text-[#F2F2F2]">Downloads</h2>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 Technic
               </a>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 CurseForge
               </a>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 MMC/Prism
               </a>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 Client ZIPs
               </a>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 Server ZIPs
               </a>
             </div>
             <div className="space-y-2">
               <h2 className="text-[#F2F2F2]">About</h2>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 Team
               </a>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 FAQ
               </a>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 Wiki
               </a>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 Changelog
               </a>
             </div>
             <div className="space-y-2">
               <h2 className="text-[#F2F2F2]">Resources</h2>
-              <a href="mod-list" className="block text-[#999999]">
+              <a href="mod-list" className="block text-text-400">
                 Mod List
               </a>
-              <a href="server" className="block text-[#999999]">
+              <a href="server" className="block text-text-400">
                 Server List
               </a>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 Documentation
               </a>
             </div>
             <div className="space-y-2">
               <h2 className="text-[#F2F2F2]">Community</h2>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 Discord Server
               </a>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 GitHub
               </a>
-              <a href="#" className="block text-[#999999]">
+              <a href="#" className="block text-text-400">
                 Bug Reports
               </a>
             </div>
